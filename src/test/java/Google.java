ccc79f03d9056298ef78c090d2ec89181844a97b
Павленков Николай <npavlenkov@gmail.com>
2021-04-26 10:17:12 +0300

    done

---
+++ src/test/java/Google.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class Google {
+
+    @Test
+    void selenideSearchTest() {
+        // Открыть google
+        open("https://www.google.com/");
+
+        // Ввести Selenide в поиск
+        $(byName("q")).setValue("Selenide").pressEnter();
+
+        // Проверить, что Selenide появился в результатах поиска
+        $("#search").shouldHave(text("selenide.org"));
+    }
+}
